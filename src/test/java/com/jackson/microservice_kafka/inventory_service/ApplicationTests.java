f7d8b70af610a595bf99708d92c2b0ec628010f9
jackson1404 <jackson14042004@gmail.com>
2025-07-06 22:27:35 +0630

    first commit

---
+++ src/test/java/com/jackson/microservice_kafka/inventory_service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackson.microservice_kafka.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
