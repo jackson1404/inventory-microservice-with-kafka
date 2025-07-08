d7f6da515fabcfe57c18ea0c151acb17668fb656
Win Min Thant <jackson14042004@gmail.com>
2025-07-08 15:25:44 +0630

    added findlowstock repo

--- src/main/java/com/jackson/microservice_kafka/inventory_service/repository/ProductRepository.java
+++ src/main/java/com/jackson/microservice_kafka/inventory_service/repository/ProductRepository.java
@@ -2,12 +2,17 @@ package com.jackson.microservice_kafka.inventory_service.repository;
 
 import com.jackson.microservice_kafka.inventory_service.model.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
     Optional<ProductEntity> findByProductId(String productId);
+
+    @Query("SELECT p FROM ProductEntity p WHERE p.productQuantity <10")
+    List<ProductEntity> findLowStockItems();
 }
--- src/main/java/com/jackson/microservice_kafka/inventory_service/schedular/InventoryCheckSchedular.java
+++ src/main/java/com/jackson/microservice_kafka/inventory_service/schedular/InventoryCheckSchedular.java
@@ -6,11 +6,15 @@
  * *************************************************************/
 package com.jackson.microservice_kafka.inventory_service.schedular;
 
+import com.jackson.microservice_kafka.inventory_service.model.ProductEntity;
+import com.jackson.microservice_kafka.inventory_service.repository.ProductRepository;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * InevntoryCheckSchedular Class.
@@ -26,8 +30,13 @@ public class InventoryCheckSchedular {
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Scheduled(fixedRate = 10000)
     public void checkStockLevel(){
+
+        List<ProductEntity> productEntities = productRepository.findLowStockItems();
 
     }
 
